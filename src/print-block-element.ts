d10aa165c91c84dfd122effb3f51704e903cab60
David Ng <david.ng.dev@gmail.com>
2025-09-01 23:45:46 +0100

    Refactor: Replace my-element with wc-print-block
    Updates index.html to use the new web component. Removes the old
    my-element.ts file. Adds the new wc-print-block.ts file.

---
+++ src/print-block-element.ts
@@ -0,0 +1,29 @@
+import { LitElement, css, html } from "lit";
+import { customElement } from "lit/decorators.js";
+
+@customElement("wc-print-block")
+export class PrintBlockElement extends LitElement {
+  static override styles = css`
+    :host {
+      max-width: 1280px;
+      margin: 0 auto;
+      padding: 2rem;
+      text-align: center;
+    }
+  `;
+
+  override render() {
+    return html`
+      <slot name="title">Default Title</slot>
+      <div>Middle</div>
+      <slot name="content">Default Content</slot>
+      <slot>Default</slot>
+    `;
+  }
+}
+
+declare global {
+  interface HTMLElementTagNameMap {
+    "wc-print-block": PrintBlockElement;
+  }
+}
